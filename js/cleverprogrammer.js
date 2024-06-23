ad55cc3f622ce9cde80a7e1520e4db55856859f0
Liam Reeves <wesleyliam005@gmail.com>
2024-06-24 01:33:39 +0300

    learnt map()

---
+++ js/cleverprogrammer.js
@@ -0,0 +1,10 @@
+// num1 ++ = Increase num 1 by 1
+// Increment or decrement by ten num += 10
+
+// num1 -- = Decrease num 1 by 1
+
+
+function fun(){
+    alert("Hello Liam");
+};
+fun();
---
+++ js/map.js
@@ -0,0 +1,11 @@
+
+
+
+const numberArray =[2,3,4,6,8]
+
+const doubleArray =numberArray.map(numberItem =>{
+    return numberItem * 2;
+
+}); 
+
+console.log(doubleArray);
